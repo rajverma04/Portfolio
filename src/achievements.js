073c28e9419fb3dc5e6866d49b3dcdfd21eca7b2
Raj Verma <rajkumargrd56@gmail.com>
2026-08-24 04:35:23 +0530

    feat: hover effect fixed

--- src/achievements.js
+++ src/achievements.js
@@ -44,7 +44,7 @@ export default function Achievements() {
               target="_blank"
               rel="noreferrer"
               key={title}
-              className="clay-card clay-panel block"
+              className="clay-card achievement-card clay-panel block"
               initial={{ opacity: 0, y: 20 }}
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ once: true }}
